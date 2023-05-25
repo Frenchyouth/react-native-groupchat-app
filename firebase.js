6ae233c8559a1af82eb64f6cd46049f0e3c58cf6
Andre French <109189379+Frenchyouth@users.noreply.github.com>
2023-05-24 20:05:35 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,7 @@ import "firebase/auth";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBnqDzDuzBC9TAEEaBEwE5Uwg2tPwLr_7s",
-  authDomain: "signalclone-7a189.firebaseapp.com",
-  projectId: "signalclone-7a189",
-  storageBucket: "signalclone-7a189.appspot.com",
-  messagingSenderId: "151722435017",
-  appId: "1:151722435017:web:e722c6d04103e4612e8818"
+//  firebase config here
 };
 
 let app;
